a9f97a25d1e9385bb3ebcba9a0f39f3a2e3a5eb6
TL <tl_mybankproj@gmail.com>
2021-07-26 19:02:09 +0530

    Date updated in welcome file ... by Dev-01

--- MyBank/src/com/vcs/home/Welcome.java
+++ MyBank/src/com/vcs/home/Welcome.java
@@ -1,9 +1,11 @@
 package com.vcs.home;
 
+import java.util.Date;
+
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To GIT Hub Repo Demo.....!");
+		System.out.println("Welcome To GIT Hub Repo Demo.....!,today Is : " + new Date());
 
 	}
 
