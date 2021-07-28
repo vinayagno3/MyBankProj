8ce59208347ba9112fd44589c69696b67366fb78
TL <tl_mybankproj@gmail.com>
2021-07-28 13:45:17 +0530

    SavingsAccount.getInf()added & test case is also added to project, by Dev-01.

--- MyBank/src/com/vcs/home/Welcome.java
+++ MyBank/src/com/vcs/home/Welcome.java
@@ -2,10 +2,13 @@ package com.vcs.home;
 
 import java.util.Date;
 
+import com.vcs.savings.SavingsAccount;
+
 public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To GIT Hub Repo Demo.....!,today Is : " + new Date());
+		System.out.println(SavingsAccount.getInfo());
 
 	}
 
---
+++ MyBank/test/TestSavingsAccount.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.vcs.savings.SavingsAccount;
+
+public class TestSavingsAccount 
+{
+
+	@Test
+	public void testGetInfo()
+	{
+		assertFalse(SavingsAccount.getInfo().equals(""));
+		
+	}
+}
