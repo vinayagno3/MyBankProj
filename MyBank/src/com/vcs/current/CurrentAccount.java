67fe895d3e5f846fe1b964ac6afde35e1e4d1aad
DEV-02 <dev02_mybankproj@gmail.com>
2021-07-30 18:52:51 +0530

    Dev-01: current a/c added

---
+++ MyBank/src/com/vcs/current/CurrentAccount.java
@@ -0,0 +1,11 @@
+package com.vcs.current;
+
+public class CurrentAccount 
+{
+	
+	public static String getInfo()
+	{
+		return "This is current A/C Info Page....!!";
+	}
+
+}
--- MyBank/src/com/vcs/home/Welcome.java
+++ MyBank/src/com/vcs/home/Welcome.java
@@ -2,6 +2,7 @@ package com.vcs.home;
 
 import java.util.Date;
 
+import com.vcs.current.CurrentAccount;
 import com.vcs.savings.SavingsAccount;
 
 public class Welcome {
@@ -9,7 +10,8 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome To GIT Hub Repo Demo.....!,today Is : " + new Date());
 		System.out.println(SavingsAccount.getInfo());
-
+		System.out.println(CurrentAccount.getInfo());
+		
 	}
 
 }
